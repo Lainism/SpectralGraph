7ee296af5aeb15b5b86851a4061ac71e58c8a5f0
Mäkinen Minttu <makinem10@ruokki.org.aalto.fi>
2018-11-29 13:36:58 +0200

    k-means

--- spectral.py
+++ spectral.py
@@ -1,8 +1,17 @@
 import networkx as nx
+import scipy
+from scipy.cluster import vq as vq
+from scipy.sparse import csgraph as cs
+
 path = "ca-GrQc.txt"
 
 fh=open(path, 'rb')
 G=nx.read_edgelist(fh)
 fh.close()
-print(len(G.nodes()))
-print(len(G.edges()))
+L = nx.normalized_laplacian_matrix(G)
+w, v = scipy.sparse.linalg.eigsh(L)
+white = vq.whiten(v)
+means = vq.kmeans2(white, 2)
+#print(len(G.nodes()))
+#print(len(G.edges()))
+print (v[:,0])
