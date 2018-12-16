c5bf765406075be8818fa92480d894dd0b6cd7f2
Mäkinen Minttu <makinem10@iibis.org.aalto.fi>
2018-12-16 13:33:01 +0200

    header handling

--- spectral.py
+++ spectral.py
@@ -6,12 +6,22 @@ from scipy.sparse import csgraph as cs
 path = "data/ca-GrQc.txt"
 
 fh=open(path, 'rb')
+
+# Read the header
+header = fh.readline()
+headers = header.split(' ')
+name = headers[1]
+verticesn = int(headers[2])
+edgesn = int(headers[3])
+k = int(headers[4])
+
+# Read the rest
 G=nx.read_edgelist(fh)
 fh.close()
 L = nx.normalized_laplacian_matrix(G)
 w, v = scipy.sparse.linalg.eigsh(L)
 white = vq.whiten(v)
-means = vq.kmeans2(white, 2)
+centroid, label = vq.kmeans2(white, k)
 #print(len(G.nodes()))
 #print(len(G.edges()))
 print (v[:,0])
